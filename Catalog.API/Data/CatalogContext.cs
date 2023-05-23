d3b5382dda89d314308b7ba0f0b11ccdfa19fd69
Guilherme Lino <guilhermelinosp@gmail.com>
2023-05-23 08:14:32 -0300

    ref: add docker-compose

--- Catalog.API/Data/CatalogContext.cs
+++ Catalog.API/Data/CatalogContext.cs
@@ -5,17 +5,91 @@ namespace Catalog.API.Data
 {
 	public class CatalogContext : ICatalogContext
 	{
+		public IMongoCollection<Product> Products { get; }
+
 		public CatalogContext(IConfiguration configuration)
 		{
-			var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:sConnectionString"));
-
-			var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
-
-			Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
-
-			CatalogContextSeed.SeedData(Products);
+			var database = InitializeDatabase(configuration);
+			Products = database.GetCollection<Product>("Products");
+			SeedDataAsync().Wait();
 		}
 
-		public IMongoCollection<Product> Products { get; }
+		private static IMongoDatabase InitializeDatabase(IConfiguration configuration)
+		{
+			var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+			return client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+		}
+
+		private async Task SeedDataAsync()
+		{
+			var hasProducts = await Products.Find(p => true).AnyAsync();
+			if (!hasProducts)
+			{
+				await Products.InsertManyAsync(CatalogContext.GetPreconfiguredProducts());
+			}
+		}
+
+		private static IEnumerable<Product> GetPreconfiguredProducts()
+		{
+			var list = new List<Product>
+			{
+				new()
+				{
+					Name = "IPhone X",
+					Summary = "Summary",
+					Description = "Description",
+					ImageFile = "product-1.png",
+					Price = 950.00M,
+					Category = "Smart Phone"
+				},
+				new()
+				{
+					Name = "Samsung 10",
+					Summary = "Summary",
+					Description = "Description",
+					ImageFile = "product-2.png",
+					Price = 840.00M,
+					Category = "Smart Phone"
+				},
+				new()
+				{
+					Name = "Huawei Plus",
+					Summary = "Summary",
+					Description = "Description",
+					ImageFile = "product-3.png",
+					Price = 650.00M,
+					Category = "White Appliances"
+				},
+				new()
+				{
+					Name = "Xiaomi Mi 9",
+					Summary = "Summary",
+					Description = "Description",
+					ImageFile = "product-4.png",
+					Price = 470.00M,
+					Category = "White Appliances"
+				},
+				new()
+				{
+					Name = "LG G 10",
+					Summary = "Summary",
+					Description = "Description",
+					ImageFile = "product-5.png",
+					Price = 320.00M,
+					Category = "Home Kitchen"
+				},
+				new()
+				{
+					Name = "Sony 10",
+					Summary = "Summary",
+					Description = "Description",
+					ImageFile = "product-6.png",
+					Price = 240.00M,
+					Category = "Home Kitchen"
+				}
+			};
+
+			return list;
+		}
 	}
 }
--- Catalog.API/Program.cs
+++ Catalog.API/Program.cs
@@ -1,20 +1,25 @@
+using Catalog.API.Data;
+using Catalog.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddApplicationInsightsTelemetry();
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
-	app.UseSwaggerUI();
+	app.UseSwaggerUI(c =>
+	{
+		c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+	});
 }
 
 app.UseAuthorization();
